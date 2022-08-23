429fe30e2cb7c68476a483e517b57a6a1abb8e60
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2022-08-23 20:02:10 +0300

    clock now updates every 1/10th second

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import {formatDate } from '@angular/common';
+// import {Subscription, timer} from 'rxjs';  
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent {
+export class ClockComponent implements OnInit {
 
-  theTime=new Date();
-  timeString='';
-  constructor() { 
-    this.timeString=formatDate(this.theTime,'dd-MM-yyyy hh:mm:ss a','en-US', '+0530');
+  public theTime=new Date();
+  public timeString='';
+  ngOnInit(): void {
+    setInterval(() => {
+      this.updateTime();
+    },100);
   }
 
+  updateTime(): void{
+      this.theTime=new Date();
+      this.timeString=formatDate(this.theTime,'dd-MM-yyyy hh:mm:ss a','en-US', '+0530');
+  }
 }
